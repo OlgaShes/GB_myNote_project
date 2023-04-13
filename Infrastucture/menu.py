2cc39a3d34c2f3c3e07b9798c7104025686d2863
OlgaShestakova <olgashestakova@gmail.com>
2023-04-13 16:35:27 +0300

    Исправлена ошибка открытия несуществующего/пустого файла

--- Infrastucture/menu.py
+++ Infrastucture/menu.py
@@ -20,37 +20,49 @@ def main_menu():
     command = ''
     while command != '6':
         v.show_text(SEPARATOR)
-        command = g.get_command()
-        match command:
-            case '1':
-                v.show_text(SEPARATOR)
-                v.print_all_notes(op.read_notes())
-            case '2':
+        notes = op.read_notes()
+        if not notes:
+            v.show_text("Ошибка, файл c заметками не найден или пуст. " + 
+                        "Для создания новой заметки нажмите 1, " + 
+                        "для прекращения работы любую другую клавишу.")
+            if input() == '1':
                 v.show_text(SEPARATOR)
                 new_note = g.get_new_note()
-                v.show_text(op.add_note(new_note))
-            case '3':
-                v.show_text(SEPARATOR)
-                date = g.get_date()
-                v.print_by_date(op.read_notes(), date) 
-            case '4':
-                v.show_text(SEPARATOR)
-                actions_with_note_by_id(op.find_by_id(g.get_answer("Введите ID заметки: ")))
-            case '5':
-                v.show_text(SEPARATOR)
-                v.show_text('Для редактирования заметки введите ее ID, '+
-                  'если не знаете ID, введите exit и воспользуйтесь '+ 
-                  'поиском по дате или просмотром всех заметок')
-                answ = g.get_answer('')
-                if answ != 'exit':
-                    actions_with_note_by_id(op.find_by_id(answ))
-            case 'help':
-                v.show_text(SEPARATOR)
-                v.show_text(INSTRUCTIONS)
-            case '6':
-                v.show_text('Спасибо за использование нашего приложения! До свидания!')
+                v.show_text(op.create_note(new_note))
+            else:
+               command = '6' 
+        else:
+            command = g.get_command()
+            match command:
+                case '1':
+                    v.show_text(SEPARATOR)
+                    v.print_all_notes(notes)
+                case '2':
+                    v.show_text(SEPARATOR)
+                    new_note = g.get_new_note()
+                    v.show_text(op.add_note(notes, new_note))
+                case '3':
+                    v.show_text(SEPARATOR)
+                    date = g.get_date()
+                    v.print_by_date(notes, date) 
+                case '4':
+                    v.show_text(SEPARATOR)
+                    actions_with_note_by_id(notes, op.find_by_id(notes, g.get_answer("Введите ID заметки: ")))
+                case '5':
+                    v.show_text(SEPARATOR)
+                    v.show_text('Для редактирования заметки введите ее ID, '+
+                    'если не знаете ID, введите exit и воспользуйтесь '+ 
+                    'поиском по дате или просмотром всех заметок')
+                    answ = g.get_answer('')
+                    if answ != 'exit':
+                        actions_with_note_by_id(notes, op.find_by_id(notes, answ))
+                case 'help':
+                    v.show_text(SEPARATOR)
+                    v.show_text(INSTRUCTIONS)
+                case '6':
+                    v.show_text('Спасибо за использование нашего приложения! До свидания!')
 
-def actions_with_note_by_id(note: dict):
+def actions_with_note_by_id(notes:dict, note: dict):
     v.print_note(note)
     if note:
         id, note_body = note.popitem()
@@ -58,13 +70,13 @@ def actions_with_note_by_id(note: dict):
         match operation:
             case '1':
                 # изменить заметку
-                v.show_text(op.change_note(id, g.get_new_title(), g.get_new_body()))
+                v.show_text(op.change_note(notes, id, g.get_new_title(), g.get_new_body()))
             case '2':
                 # удалить заметку
-                v.show_text(op.delete_note(id))
+                v.show_text(op.delete_note(notes, id))
             case '3':
                 # найти новую заметку
-                actions_with_note_by_id(op.find_by_id(g.get_answer("Введите ID заметки: ")))
+                actions_with_note_by_id(notes, op.find_by_id(g.get_answer("Введите ID заметки: ")))
             case 'exit':
                 # выход из редактирования
                 return
--- Infrastucture/operations.py
+++ Infrastucture/operations.py
@@ -3,18 +3,20 @@ from datetime import datetime
 import config as cn
 
 def read_notes() -> dict:
-    with open(cn.FILE_PATH, 'r', encoding='utf-8') as data:
-        note_json = data.read()
-    notes = json.loads(note_json, parse_int=int)
-    return notes
+    try:
+        with open(cn.FILE_PATH, 'r', encoding='utf-8') as data:
+            note_json = data.read()
+        notes = json.loads(note_json, parse_int=int)
+        return notes
+    except FileNotFoundError:
+        return
 
 def save_notes(notes: dict):
     note_json = json.dumps(notes, ensure_ascii=False)
     with open(cn.FILE_PATH, 'w', encoding='utf-8') as data:
         data.write(note_json)
 
-def add_note(new_note: dict) -> str:
-    notes = read_notes()
+def add_note(notes: dict, new_note: dict) -> str:
     note_id = len(notes)
     while str(note_id) in notes.keys():
         note_id += 1
@@ -22,20 +24,24 @@ def add_note(new_note: dict) -> str:
     save_notes(notes)
     return 'Заметка успешно сохранена'
 
-def find_by_id(id: str) -> dict:
+def create_note(new_note: dict) -> str:
+    note_id = 1
+    notes = {}
+    notes[note_id] = new_note
+    save_notes(notes)
+    return 'Заметка успешно сохранена'
+
+def find_by_id(notes: dict, id: str) -> dict:
     id = str(id)
-    notes = read_notes()
     if id in notes:
         return {id: notes[id]}
 
-def delete_note(id: str) -> str:
-    notes = read_notes()
+def delete_note(notes: dict, id: str) -> str:
     notes.pop(id)
     save_notes(notes)
     return "Заметка удалена"
 
-def change_note(id: str, new_title: str, new_body: str) -> str:
-    notes = read_notes()
+def change_note(notes: dict, id: str, new_title: str, new_body: str) -> str:
     # Внести изменения
     if new_title:
         notes[id]['note_title'] = new_title
